2e25a6b90f8ba9ab589aabd6b516263a5e0035a6
Riccardo <richi-97@live.it>
2025-11-07 12:04:39 +0100

    iniziata logica home

--- scripts/crudazon.js
+++ scripts/crudazon.js
@@ -1,3 +1,26 @@
 const authkey =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTBkYmE2MmY0YmQ0NzAwMTU4NWIxZTciLCJpYXQiOjE3NjI1MDczNjIsImV4cCI6MTc2MzcxNjk2Mn0.vQzChT97YTOe2A45k6nIDMqA-gNygnjtO0nK5x1s4vk";
 const endpoint = "https://striveschool-api.herokuapp.com/api/product/";
+
+getProducts = function () {
+  fetch(endpoint, {
+    method: "GET", // metodo GET per ottenere informazioni dal DB
+    headers: {
+      Authorization: authkey,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Errore nella risposta dal server: ${response.status}`);
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("ERRORE: ", error);
+    });
+};
+getProducts();
